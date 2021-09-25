e94630ed3a3d3cbc2becfc0a1aff9bda46a4690f
Brett Kromkamp <brettkromkamp@gmail.com>
2021-09-25 19:59:45 +0200

    Finalize status-checking functionality in the Angular app

--- client/src/app/components/loan-status/loan-status.component.ts
+++ client/src/app/components/loan-status/loan-status.component.ts
@@ -2,31 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { LoanService } from 'src/app/services/loan.service';
 
 @Component({
-  selector: 'app-loan-status',
-  templateUrl: './loan-status.component.html',
-  styleUrls: ['./loan-status.component.css']
+	selector: 'app-loan-status',
+	templateUrl: './loan-status.component.html',
+	styleUrls: ['./loan-status.component.css']
 })
 export class LoanStatusComponent implements OnInit {
 
-  currentLoan = null;
-  loanIdentifier = '';
+	currentLoan = null;
+	loanStatus = '';
+	loanIdentifier = '';
 
-  constructor(private loanService: LoanService) { }
+	constructor(private loanService: LoanService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  searchByIdentifier(): void {
-    this.loanService.read(this.loanIdentifier)
-      .subscribe(
-        loan => {
-          this.currentLoan = loan;
-          console.log(loan);
-        },
-        error => {
-          console.log(error);
-        }
-      );
-  }
+	searchByIdentifier(): void {
+		this.loanService.read(this.loanIdentifier)
+			.subscribe(
+				loan => {
+					this.currentLoan = loan;
+					this.loanStatus = loan.status;
+					console.log(loan);
+				},
+				error => {
+					this.currentLoan = null;
+					console.log(error);
+				}
+			);
+	}
 
 }
